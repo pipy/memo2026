08b58c91f8a4659ff38bfaf5d752e1f0d5ff7a85
pipy <104475419+pipy@users.noreply.github.com>
2026-07-28 17:52:42 +0900

    Protect memo routes with authentication

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\NoteController;
 use Illuminate\Support\Facades\Route;
 
-Route::redirect('/', '/notes');
-Route::resource('notes', NoteController::class)->except(['show']);
+Route::middleware('guest')->group(function (): void {
+    Route::get('/login', [AuthController::class, 'create'])->name('login');
+    Route::post('/login', [AuthController::class, 'store'])->name('login.store');
+});
+
+Route::middleware('auth')->group(function (): void {
+    Route::redirect('/', '/notes');
+    Route::resource('notes', NoteController::class)->except(['show']);
+    Route::post('/logout', [AuthController::class, 'destroy'])->name('logout');
+});
